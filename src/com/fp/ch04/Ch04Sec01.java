276135928aaa71c347cf42be9ebe9fccd1484ed4
kshneo0 <kshneo@gmail.com>
2022-03-23 08:13:00 +0900

    Ch04Sec01

---
+++ src/com/fp/ch04/Ch04Sec01.java
@@ -0,0 +1,22 @@
+package com.fp.ch04;
+
+import java.util.function.Supplier;
+
+public class Ch04Sec01 {
+
+	public static void main(String[] args) {
+		
+		Supplier<String> myStringSupplier = () -> "hello world!";
+//		System.out.println(myStringSupplier.get());
+		
+		Supplier<Double> myRandomDoubleSupplier = () -> Math.random();
+		printRandomDoubles(myRandomDoubleSupplier, 5);
+	}
+	
+	public static void printRandomDoubles(Supplier<Double> randomSupplier, int count) {
+		for(int i = 0; i < count; i++) {
+			System.out.println(randomSupplier.get());
+		}
+	}
+
+}
