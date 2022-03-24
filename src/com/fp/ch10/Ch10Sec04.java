1d1b3db770a5ddcda828b0c62f9e88c7a0ee547a
kshneo0 <kshneo@gmail.com>
2022-03-24 10:50:28 +0900

    Ch10Sec04

---
+++ src/com/fp/ch10/Ch10Sec04.java
@@ -0,0 +1,58 @@
+package com.fp.ch10;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.fp.ch10.model.User;
+import com.fp.ch10.service.EmailProvider;
+import com.fp.ch10.service.EmailSender;
+import com.fp.ch10.service.MakeMoreFriendsEmailProvider;
+import com.fp.ch10.service.VerifyYourEmailAddressEmailProvider;
+
+
+public class Ch10Sec04 {
+
+	public static void main(String[] args) {
+		User user1 = User.builder(1, "Alice")
+				.with(builder -> {
+					builder.emailAddress = "alice@fastcampus.co.kr";
+					builder.isVerified = false;
+					builder.friendUserIds = Arrays.asList(201, 202, 203, 204, 211, 212, 213, 214);
+				}).build();
+		User user2 = User.builder(2, "Bob")
+				.with(builder -> {
+					builder.emailAddress = "bob@fastcampus.co.kr";
+					builder.isVerified = true;
+					builder.friendUserIds = Arrays.asList(212, 213, 214);
+				}).build();
+		User user3 = User.builder(3, "Charlie")
+				.with(builder -> {
+					builder.emailAddress = "charlie@fastcampus.co.kr";
+					builder.isVerified = true;
+					builder.friendUserIds = Arrays.asList(201, 202, 203, 204, 211, 212);
+				}).build();
+		List<User> users = Arrays.asList(user1, user2, user3);
+		
+		EmailSender emailSender = new EmailSender();
+		EmailProvider verifyYourEmailAddressEmailProvider = new VerifyYourEmailAddressEmailProvider();
+		EmailProvider makeMoreFriendsEmailProvider = new MakeMoreFriendsEmailProvider();
+		
+		emailSender.setEmailProvider(verifyYourEmailAddressEmailProvider);
+		users.stream()
+			.filter(user -> !user.isVerified())
+			.forEach(emailSender::sendEmail);
+		
+		emailSender.setEmailProvider(makeMoreFriendsEmailProvider);
+		users.stream()
+			.filter(User::isVerified)
+			.filter(user -> user.getFriendUserIds().size() <= 5)
+			.forEach(emailSender::sendEmail);
+		
+		emailSender.setEmailProvider(user -> "'Play With Friends' email for " + user.getName());
+		users.stream()
+			.filter(User::isVerified)
+			.filter(user -> user.getFriendUserIds().size() > 5)
+			.forEach(emailSender::sendEmail);
+	}
+
+}
